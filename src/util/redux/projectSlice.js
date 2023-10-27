e19033620558ab0d79af9957b0e640eeb02f0eda
angelohague <angelohague@aol.co.uk>
2023-10-27 12:49:03 +0100

    Fixed adding tasks to redux

--- src/util/redux/projectSlice.js
+++ src/util/redux/projectSlice.js
@@ -47,40 +47,59 @@ export const projectSlice = createSlice({
             }
         },
         ADD_TASK: (state, action) => {
-            const task = action.payload
+            const task = action.payload;
+        
             // Find the current project's index
             const projectIndex = state.projects.findIndex(project => project.id === state.current.id);
-            const tasks = (state.projects[projectIndex].tasks) ? (state.projects[projectIndex].tasks) : [task]
+        
             if (projectIndex !== -1) {
                 // Create a new project object with the updated tasks array
                 const updatedProject = {
                     ...state.projects[projectIndex],
-                    tasks: tasks,
+                    tasks: [...state.projects[projectIndex].tasks, task], // Add the new task
                 };
-
+        
                 // Create a new projects array with the updated project
                 state.projects = [
                     ...state.projects.slice(0, projectIndex),
                     updatedProject,
                     ...state.projects.slice(projectIndex + 1),
                 ];
+        
+                // Update the current project to the updated one
+                state.current = updatedProject;
             }
-            // state.projects[projectIndex].tasks = (state.projects[projectIndex].tasks) ? [...state.projects[projectIndex].tasks, action.payload] : [action.payload];
-            // state.projects[projectID].tasks = [...state.projects[projectID].tasks, action.payload]
         },
+        GET_TASKS: (state) => {
+            // Find the current project's index
+            const projectIndex = state.projects.findIndex(project => project.id === state.current.id);
+
+            if (projectIndex !== -1) {
+                return state.projects[projectIndex].tasks;
+            } else {
+                return -1;
+            }
+        },      
         TOGGLE_TASKS: (state) => {
             const current = state.showTasks;
             state.showTasks = !current;
         },
         // Current Project Handlers
         SET_CURRENT_PROJECT: (state, action) => {
-            state.current = action.payload;
+            const projectId = action.payload.id;
+            // Find the project with the matching ID
+            const matchingProject = state.projects.find(project => project.id === projectId);
+            // Set the current project to the matching project or null if not found
+            state.current = matchingProject || null;
         },
+        // SET_CURRENT_PROJECT: (state, action) => {
+        //     state.current = action.payload;
+        // },
         CLEAR_CURRENT_PROJECT: (state) => {
             state.current = null;
         }
     }
 });
 
-export const { ADD_PROJECT, SET_PROJECTS, TOGGLE_TASKS, SET_TASKS, ADD_TASK, DELETE_PROJECT, SET_CURRENT_PROJECT, CLEAR_CURRENT_PROJECT } = projectSlice.actions;
+export const { ADD_PROJECT, SET_PROJECTS, TOGGLE_TASKS, SET_TASKS, GET_TASKS, ADD_TASK, DELETE_PROJECT, SET_CURRENT_PROJECT, CLEAR_CURRENT_PROJECT } = projectSlice.actions;
 export default projectSlice.reducer;
